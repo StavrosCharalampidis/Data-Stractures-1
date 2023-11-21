30676db5241ec33c96780116d739faaa2e3f2937
StavrosCharalampidis <stavroscharalampidis1@gmail.com>
2023-11-21 02:17:31 +0200

    C auto msg

--- main.c
+++ main.c
@@ -1,6 +1,23 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+
+typedef struct Singly_Linked_List {
+    int data;
+    struct S_Node *next;
+} S_Node;
+
+
+
+
+typedef struct Doubly_Linked_List {
+  int data;
+  struct D_Node *next;
+  struct D_Node *prev;
+} D_Node;
+
+
+
 int main(int argc, char const *argv[])
 {
     /* code */
