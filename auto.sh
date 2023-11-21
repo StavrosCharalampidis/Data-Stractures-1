3e94b5b609de11483247884d276dde904419f32c
StavrosCharalampidis <stavroscharalampidis1@gmail.com>
2023-11-21 11:19:39 +0200

    Automate shell script

--- auto.sh
+++ auto.sh
@@ -2,10 +2,11 @@
 
 git add Icsd_Lab02/.
 git commit -m "C Exercise"
-
+git push -u origin Test
 
 git add auto.sh
 git commit -m "Automate shell script"
+git push -u origin Test
 
 git add .
 git commit -m "extesion"
