79db31330e853f4459ecb6c060226848b6ee5918
StavrosCharalampidis <stavroscharalampidis1@gmail.com>
2023-11-21 02:35:33 +0200

    C auto msg

--- main.c
+++ main.c
@@ -1,24 +1,38 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-typedef struct Doubly_Linked_List
-{
-    int data;
-    struct D_Node *next;
-    struct D_Node *prev;
-} S_Node;
 
 typedef struct Singly_Linked_List
 {
     int data;
-    D_Node *next;
+    struct Singly_Linked_List *next;
+} S_Node;
+
+
+typedef struct Doubly_Linked_List
+{
+    int data;
+    struct Doubly_Linked_List *next;
+    struct Doubly_Linked_List *prev;
 } D_Node;
 
 
+void addNodeSingle(){}
+void addDescending(){}
+void displayList(){}
+void countList(){}
+void deleteNode(){}
+void deleteFirst(){}
+
+void appendLists(){}
+void addNodeDouble(){}
+void swapElements1(){}
+void swapElements2(){}
+void ReversedisplayList(){}
 
 
 // function to print the linked list
-void display(D_Node *node)
+void display(S_Node *node)
 {
     while (node != NULL)
     {
@@ -31,16 +45,16 @@ int main(int argc, char const *argv[])
 {
     // creating 4 pointers of type D_Node
     // So these can point to address of struct type variable
-    D_Node *head = NULL;
-    D_Node *node2 = NULL;
-    D_Node *node3 = NULL;
-    D_Node *node4 = NULL;
+    S_Node *head = NULL;
+    S_Node *node2 = NULL;
+    S_Node *node3 = NULL;
+    S_Node *node4 = NULL;
 
     // allocate 3 nodes in the heap
-    head = (D_Node *)malloc(sizeof(D_Node));
-    node2 = (D_Node *)malloc(sizeof(D_Node));
-    node3 = (D_Node *)malloc(sizeof(D_Node));
-    node4 = (D_Node *)malloc(sizeof(D_Node));
+    head = (S_Node *)malloc(sizeof(S_Node));
+    node2 = (S_Node *)malloc(sizeof(S_Node));
+    node3 = (S_Node *)malloc(sizeof(S_Node));
+    node4 = (S_Node *)malloc(sizeof(S_Node));
 
     head->data = 10;    // data set for head node
     head->next = node2; // next pointer assigned to address of node2
