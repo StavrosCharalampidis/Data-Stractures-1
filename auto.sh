0e4c7b45dbe19e319a6317835552bdf7add0b952
StavrosCharalampidis <stavroscharalampidis1@gmail.com>
2023-11-21 11:31:29 +0200

    Automate shell script

--- auto.sh
+++ auto.sh
@@ -4,12 +4,7 @@ git add vscode/*
 git commit -m "Extensions"
 git push -u origin Test
 
-for i in Icsd_Lab02/*
-do
-    git add "$i"
-    git commit -m "C Exercise"
-    git push -u origin Test
-done
+
 
 git add auto.sh
 git commit -m "Automate shell script"
