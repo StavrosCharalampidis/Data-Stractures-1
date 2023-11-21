5ddd36649632c7ec258ea96d71dcf5f856a575f5
StavrosCharalampidis <stavroscharalampidis1@gmail.com>
2023-11-21 11:28:05 +0200

    Automate shell script

--- auto.sh
+++ auto.sh
@@ -1,19 +1,16 @@
 #!bin/bash
 
-for i in {2..10}
-do
-    echo "output: $i"
-done
-
-git add Icsd_Lab02/*
+git add .vscode
 git commit -m "C Exercise"
 git push -u origin Test
 
+for i in Icsd_Lab02/*
+do
+    git add "$i"
+    git commit -m "C Exercise"
+    git push -u origin Test
+done
+
 git add auto.sh
 git commit -m "Automate shell script"
-git push -u origin Test
-
-git add .
-git commit -m "extesion"
-
 git push -u origin Test
