105a9c609982a978a11b1b15b9f81ad563ce135e
StavrosCharalampidis <stavroscharalampidis1@gmail.com>
2023-11-22 14:46:35 +0200

    📚 C Exercise 📚

--- Icsd_Lab02/Exercise23.c
+++ Icsd_Lab02/Exercise23.c
@@ -1,8 +1,80 @@
-// icsd
-#include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
-int main(int argc, char const *argv[]) {
+#define MAX_SIZE 50
+
+void push(char stack[][MAX_SIZE], int *top, char *item);
+void pop(char stack[][MAX_SIZE], int *top, char *item);
+
+int main() {
+    char products[MAX_SIZE][MAX_SIZE];
+    int prices[MAX_SIZE];
+    int top = -1;
+    int total = 0;
+    int undo_count = 0;
+    char operation[5];
+    char product[MAX_SIZE];
+    int price;
     
+    while (1) {
+        printf("Enter operation (BUY, UNDO, END): ");
+        scanf("%s", operation);
+        
+        if (strcmp(operation, "BUY") == 0) {
+            if (top == MAX_SIZE - 1) {
+                printf("The shopping cart is full.\n");
+            } else {
+                printf("Enter product name: ");
+                scanf("%s", product);
+                printf("Enter product price: ");
+                scanf("%d", &price);
+                
+                push(products, &top, product);
+                prices[top] = price;
+                total += price;
+            }
+        } else if (strcmp(operation, "UNDO") == 0) {
+            if (top == -1) {
+                printf("The shopping cart is empty.\n");
+            } else {
+                char last_product[MAX_SIZE];
+                pop(products, &top, last_product);
+                int last_price = prices[top + 1];
+                total -= last_price;
+            }
+            
+            if (undo_count == 1) {
+                undo_count = 0;
+            } else {
+                undo_count++;
+            }
+        } else if (strcmp(operation, "END") == 0) {
+            break;
+        } else {
+            printf("Invalid operation.\n");
+        }
+    }
+    
+    if (top == -1) {
+        printf("No purchases were made.\n");
+    } else {
+        printf("Number of products: %d\n", top + 1);
+        printf("Total value: %d\n", total);
+    }
+    
+    if (undo_count > 0) {
+        printf("Number of consecutive UNDO operations: %d\n", undo_count);
+    }
+
     return 0;
+}
+
+void push(char stack[][MAX_SIZE], int *top, char *item) {
+    (*top)++;
+    strcpy(stack[*top], item);
+}
+
+void pop(char stack[][MAX_SIZE], int *top, char *item) {
+    strcpy(item, stack[*top]);
+    (*top)--;
 }
--- Icsd_Lab02/Exercise24.c
+++ Icsd_Lab02/Exercise24.c
@@ -1,8 +1,102 @@
-// icsd
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-int main(int argc, char const *argv[]) {
+#define QUEUE_SIZE 30
+
+typedef struct {
+    char name[50];
+    int clientNumber;
+    int entranceTime;
+} Customer;
+
+Customer queue[QUEUE_SIZE];
+int front = 0;
+int rear = 0;
+int count = 0;
+
+void enqueue(Customer customer) {
+    if (count == QUEUE_SIZE) {
+        printf("Queue is full. Cannot add customer.\n");
+        return;
+    }
+    queue[rear] = customer;
+    rear = (rear + 1) % QUEUE_SIZE;
+    count++;
+    printf("Customer %s entered the queue.\n", customer.name);
+}
+
+Customer dequeue() {
+    if (count == 0) {
+        printf("Queue is empty. No customer to remove.\n");
+        Customer emptyCustomer;
+        strcpy(emptyCustomer.name, "");
+        emptyCustomer.clientNumber = -1;
+        emptyCustomer.entranceTime = -1;
+        return emptyCustomer;
+    }
+    Customer customer = queue[front];
+    front = (front + 1) % QUEUE_SIZE;
+    count--;
+    printf("Customer %s exited the queue.\n", customer.name);
+    return customer;
+}
+
+void displayCustomers() {
+    printf("%-10s %-15s %-10s\n", "Number", "Name", "EntranceTime");
+    for (int i = 0; i < count; i++) {
+        int index = (front + i) % QUEUE_SIZE;
+        printf("%-10d %-15s %-10d\n", queue[index].clientNumber, queue[index].name, queue[index].entranceTime);
+    }
+}
+
+int getCustomerCount() {
+    return count;
+}
+
+int main() {
+    int choice;
+    int clientNumber = 1;
+    
+    while (1) {
+        printf("\nMENU\n");
+        printf("=====\n");
+        printf("1. Enter customer into the queue.\n");
+        printf("2. Exit customer from the queue.\n");
+        printf("3. Display customer details in the queue.\n");
+        printf("4. Number of customers in the queue.\n");
+        printf("0. Exit\n");
+
+        printf("Enter your choice: ");
+        scanf("%d", &choice);
+
+        Customer customer;
+        
+        switch (choice) {
+            case 1:
+                printf("Enter customer name: ");
+                scanf(" %[^\n]s", customer.name);
+                customer.clientNumber = clientNumber++;
+                customer.entranceTime = time(NULL);
+                enqueue(customer);
+                break;
+            case 2:
+                customer = dequeue();
+                break;
+            case 3:
+                displayCustomers();
+                break;
+            case 4:
+                printf("Number of customers in the queue: %d\n", getCustomerCount());
+                break;
+            case 0:
+                printf("Exiting program...\n");
+                exit(0);
+            default:
+                printf("Invalid choice. Please try again.\n");
+                break;
+        }
+    }
     
     return 0;
 }
