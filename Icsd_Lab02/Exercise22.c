0e78781fc17ef53bb86bc745de4320404d0eb19f
StavrosCharalampidis <stavroscharalampidis1@gmail.com>
2023-11-22 14:30:28 +0200

    📚 C Exercise 📚

--- Icsd_Lab02/Exercise22.c
+++ Icsd_Lab02/Exercise22.c
@@ -2,6 +2,22 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+
+typedef struct Doubly_Linked_List
+{
+    const char *data;
+    struct Doubly_Linked_List *next;
+    struct Doubly_Linked_List *prev;
+} D_Node;
+
+typedef struct Music_Library{
+    const char *title;
+    const char *artist[10];
+    const char *eidos;
+    double time;
+
+} Music_Library;
+
 int main(int argc, char const *argv[]) {
 
     return 0;
