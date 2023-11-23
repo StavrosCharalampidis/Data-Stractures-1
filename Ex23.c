46010a5cdbcd3329e1a48b783ec8dbc284b0e523
StavrosCharalampidis <stavroscharalampidis1@gmail.com>
2023-11-23 08:17:58 +0200

    Random Commits

--- Ex23.c
+++ Ex23.c
@@ -11,8 +11,55 @@ typedef struct Product {
 
 // Δομή για το καλάθι αγορών
 typedef struct ShoppingCart {
+    int undoCount;
     Product *top;
 } ShoppingCart;
+
+void buyProduct(ShoppingCart *cart, const char *name, float value);
+void undoLastPurchase(ShoppingCart *cart);
+void undoLastPurchase(ShoppingCart *cart);
+void displayCartInfo(ShoppingCart *cart);
+void freeShoppingCart(ShoppingCart *cart);
+
+int main() {
+    ShoppingCart cart;
+    Product product;
+    cart.top = NULL;
+
+    char action[5];
+
+    
+    
+    do {
+        printf("\nΕπιλέξτε κίνηση (BUY, UNDO, END): ");
+        scanf("%s", action);
+
+        if (strcmp(action, "BUY") == 0) {
+            product.name;
+            float value;
+
+            printf("Εισάγετε όνομα προϊόντος: ");
+            scanf("%s", product.name);
+
+            printf("Εισάγετε τιμή προϊόντος: ");
+            scanf("%f", &value);
+
+            buyProduct(&cart, product.name, value);
+        } else if (strcmp(action, "UNDO") == 0) {
+            undoLastPurchase(&cart);
+        } else if (strcmp(action, "END") != 0) {
+            printf("Μη έγκυρη ενέργεια. Προσπαθήστε ξανά.\n");
+        }
+    } while (strcmp(action, "END") != 0);
+
+    displayCartInfo(&cart);
+
+    // Αποδέσμευση μνήμης του καλαθιού αγορών
+    freeShoppingCart(&cart);
+
+    return 0;
+}
+
 
 // Συνάρτηση για εισαγωγή προϊόντος στο καλάθι
 void buyProduct(ShoppingCart *cart, const char *name, float value) {
@@ -30,6 +77,7 @@ void buyProduct(ShoppingCart *cart, const char *name, float value) {
     printf("Προστέθηκε στο καλάθι αγορών: %s, Τιμή: %.2f\n", name, value);
 }
 
+
 // Συνάρτηση για αφαίρεση τελευταίου προϊόντος από το καλάθι
 void undoLastPurchase(ShoppingCart *cart) {
     if (cart->top != NULL) {
@@ -42,23 +90,28 @@ void undoLastPurchase(ShoppingCart *cart) {
     }
 }
 
+
+
 // Συνάρτηση για εμφάνιση πληροφοριών του καλαθιού αγορών
 void displayCartInfo(ShoppingCart *cart) {
     if (cart->top != NULL) {
         float totalValue = 0;
-        int undoCount = 0;
+        int Count = 0;
+        int UndoCount = 1;
 
         Product *current = cart->top;
         while (current != NULL) {
             totalValue += current->value;
-            undoCount++;
+            Count++;
+            UndoCount++;
             current = current->next;
         }
 
-        printf("Πλήθος προϊόντων στο καλάθι αγορών: %d\n", undoCount);
+        printf("Πλήθος προϊόντων στο καλάθι αγορών: %d\n", Count);
         printf("Συνολική αξία: %.2f\n", totalValue);
-        printf("Πλήθος αναιρέσεων: %d\n", undoCount - 1);
-    } else {
+        printf("Πλήθος αναιρέσεων: %d\n", UndoCount - 1);
+    } 
+    else {
         printf("Δεν πραγματοποιήθηκαν αγορές.\n");
     }
 }
@@ -71,40 +124,4 @@ void freeShoppingCart(ShoppingCart *cart) {
         current = current->next;
         free(temp);
     }
-}
-
-int main() {
-    ShoppingCart cart;
-    cart.top = NULL;
-
-    char action[5];
-
-    do {
-        printf("\nΕπιλέξτε κίνηση (BUY, UNDO, END): ");
-        scanf("%s", action);
-
-        if (strcmp(action, "BUY") == 0) {
-            char product[50];
-            float value;
-
-            printf("Εισάγετε όνομα προϊόντος: ");
-            scanf("%s", product);
-
-            printf("Εισάγετε τιμή προϊόντος: ");
-            scanf("%f", &value);
-
-            buyProduct(&cart, product, value);
-        } else if (strcmp(action, "UNDO") == 0) {
-            undoLastPurchase(&cart);
-        } else if (strcmp(action, "END") != 0) {
-            printf("Μη έγκυρη ενέργεια. Προσπαθήστε ξανά.\n");
-        }
-    } while (strcmp(action, "END") != 0);
-
-    displayCartInfo(&cart);
-
-    // Αποδέσμευση μνήμης του καλαθιού αγορών
-    freeShoppingCart(&cart);
-
-    return 0;
 }
