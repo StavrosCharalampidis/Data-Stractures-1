74615a060929866ad6c479facc37d3f036631fd6
StavrosCharalampidis <stavroscharalampidis1@gmail.com>
2023-12-03 11:27:13 +0200

    📚 C Exercise 📚

--- Icsd22239_lab03/Exercise31.cpp
+++ Icsd22239_lab03/Exercise31.cpp
@@ -9,38 +9,46 @@ private:
     Node<T> *Left, *Right;
 
 public:
-    Node(T val) : data(val), Left(nullptr), Right(nullptr) {}
-
-    Node<T> * insert_node(Node<T> *root, T data)
+    Node(T val) : data(val), Left(NULL), Right(NULL) {};
+    Node<T> * insert_node(Node<T> **root, T data)
     {
-        if (root == nullptr)
+        if (*root == NULL)
         {
             Node<T> *new_node = new Node<T>(data);
-            new_node->Left = nullptr;
-            new_node->Right = nullptr;
+            new_node->Left = NULL;
+            new_node->Right = NULL;
             return new_node;
         }
-        if (data < root->data)
+        if (data < (*root)->data)
         {
-            root->Left = insert_node(root->Left, data);
+            (*root)->Left = insert_node(&(*root)->Left, data);
         }
         else
         {
-            root->Right = insert_node(root->Right, data);
+            (*root)->Right = insert_node(&(*root)->Right, data);
         }
 
-        return root;
-    }
+        return *root;
+    };
 
     void inorder(Node<T> *root)
     {
-        if (root != nullptr)
+        if (root != NULL)
         {
             inorder(root->Left);
             printf("%d ", root->data);
             inorder(root->Right);
         }
-    }
+    };
+
+    void printPreOrder(Node<T> *root){
+        if (root != NULL){
+            printf("%d ", root->data);
+            printPreOrder(root->Left);
+            printPreOrder(root->Right);
+        }
+
+    };
 
 };
 
@@ -54,11 +62,14 @@ public:
 
 int main(int argc, char const *argv[])
 {
-    Node<int> *root = new Node<int>(60);
-    (*root).insert_node(root, 20);
-    (*root).insert_node(root, 50);
-    (*root).insert_node(root, 60);
+    Node<int> *root = new Node<int>(10);
+    (*root).insert_node(&root, 20);
+    (*root).insert_node(&root, 0);
+    (*root).insert_node(&root, 60);
+    printf("inorder \n");
     (*root).inorder(root);
-    
+    printf("\npreorder \n");
+    (*root).printPreOrder(root);
+    printf("\n");
     return 0;
 }
